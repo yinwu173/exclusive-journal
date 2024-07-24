8bc374fa0d2414a2866e1a11da68505c504b6923
yinwu173 <yin.wu173@gmail.com>
2024-07-23 20:37:02 -0700

    added alert method to javascript form

--- assets/javascript/form.js
+++ assets/javascript/form.js
@@ -3,8 +3,6 @@ const textInput = document.querySelector('#text-input');
 const submitBtn = document.querySelector('#submit');
 
 
-
-
 // step 2: create event listener for the submit button, listener sends to function one
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -14,6 +12,17 @@ submitBtn.addEventListener('click', function (event) {
         title: title.value,
         content: content.value,
     };
+
+    // check if any of the forms are empty
+    if (!username || !title || !content) {
+        // prevent the form for resubmitting and clearing the inputed data
+        event.preventDefault()
+        
+        // if empty, prompt an alert
+        alert("Please fill in all sections of the form.");
+        return;
+    }
+
     // in order to store in local storage, it needs to be simple data type so we used JSON
     // sets submission to local storage
     localStorage.setItem('blogDetail', JSON.stringify(blogDetail));
@@ -22,11 +31,3 @@ submitBtn.addEventListener('click', function (event) {
     // when submit button is clicked, it will redirect me to the blog page
     window.location.href = 'blog.html'
 });
-
-
-
-
-
-// step 3: Function one, take in the inputs and localStorage.setItem(). Once down, need to flow
-// to next function which is another page - blog page (redirected). In the same function, a rule: if
-// empty "return" - prompted with an alert.
